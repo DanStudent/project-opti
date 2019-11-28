1bb71cf380ad2760a4e22ab88664779e260fcbba
dan <danial.joukar@student.umons.ac.be>
2019-11-28 11:13:44 +0100

    added the class cost

---
+++ src/Cost.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Cost {
+    double []cost;
+    ArrayList<ArrayList<Integer>> sols[];
+    public Cost(double []cost, ArrayList<ArrayList<Integer>>[] sols){
+        this.cost = cost;
+        this.sols = sols;
+    }
+}
